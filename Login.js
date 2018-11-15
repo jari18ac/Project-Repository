611798dcf39c019421bb428b3ec37ee15212ab49
OliverUngermann <43135702+OliverUngermann@users.noreply.github.com>
2018-11-15 22:31:01 +0100

    Fixed array error + other stuff
    - Fixed signup/login error. Now properly adds the user to the array and saves the array in localstorage.
    - Added localstorage to be included in the array "users" when page load.
    
    - Fixed error so you can't register with undefined user and pass.
    - Fixed error so you can't create a user with the same username as found in the array.
    - Added styling to the login/signup page.

--- Login.js
+++ Login.js
@@ -11,10 +11,13 @@ class User {
         this.password = password;
         this.result = result;
     }
+
 }
 
-var users = [];
- //have predefined users because we arent using a database
+// The array 'users' should contain both what we already have in localstorage + the new users created
+users = JSON.parse(localStorage.getItem("localStorageUsers")) || [];
+
+ //We have a set of predifined users for the array to use for the highscore later
 users.push(new User("Mathias", "25", "Pepke", ""));
 users.push(new User("James", "24", "Richman", ""));
 users.push(new User("Oliver1", "24", "12", ""));
@@ -36,33 +39,67 @@ function signUp() {
 var createUsername = document.getElementById('createUsername').value;
 var createAge = document.getElementById('createAge').value;
 var createPassword = document.getElementById('createPassword').value;
-//We use localStorage to store the data typed in the fields
 
-users.push(new User(createUsername, createAge, createPassword));
+//We make sure that you can't create a user without any name or password
+if(createUsername.length == 0 || createPassword.length == 0){
+    // We set the resultspan with a new text and return false to get out of this function
+    document.getElementById('loginResult').innerHTML = "Please fill in the required fields";
+    return false;
+}
 
+//We loop through the array of users
+for(var i = 0; i < users.length; i++) {
+    //we create a var user to bind the array of users for easy use (below). Now we can write user instead of myUsers[i]
+     var user = users[i];
+    // We check whether the username is already taken
+    if ((createUsername == user.username )) {
+        document.getElementById('loginResult').innerHTML = "Username is already taken!";
+    
+    return false;  
+}
+}
+//We first push the new user into the 'users' array
+users.push(new User(createUsername, createAge, createPassword, ""));
+//We then store the updated array within localstorage by converting it to a string
 localStorage.setItem("localStorageUsers", JSON.stringify(users)); 
+document.getElementById('loginResult').innerHTML = '<p>Success! The account has been created</p>';
+
 
 }
 
-var myUsers = [(JSON.parse(localStorage.getItem("localStorageUsers")))];
-console.log(myUsers);
 console.log(users);
 
 // Bind the onClick-function to our own function
-submit.onclick = function(){
+function login() {
 
+//We use JSON.parse to get the data back from localstorage in its original format
+var myUsers = JSON.parse( localStorage.getItem( "localStorageUsers" ) );
+
+    console.log(myUsers);
     // Bind the two input fields and get the value
     var inputUsername = document.getElementById('username').value;
     var inputPassword = document.getElementById('password').value;
 
+     //if the username or password isnt filled out it will return the message belwo
+    if(inputUsername.length == 0 || inputPassword.length == 0){
+        // We set the resultspan with a new text and return false to get out of this function
+        document.getElementById('loginResult').innerHTML = "You need to enter a username and password in order to use our system";
+        return false;
+    }
+
 //var storedUsers = localStorage.getItem("localStorageUsers");
 // var storedUsers2 = JSON.parse(storedUsers);
 
-
+   // We loop through all our users and return true if we find a match
+   for(var i = 0; i < myUsers.length; i++) {
+    //we create a var user to bind the array of users for easy use (below). Now we can write user instead of myUsers[i]
+     var user = myUsers[i];
     // We check whether the input matches what is stored in localStorage
-    if ((inputUsername == myUsers.username && inputPassword == myUsers.password)) {
+    if ((inputUsername == user.username && (inputPassword == user.password))) {
     //We set a status to the user to be able to check whether they are logged in or not at other pages. Note that the comma (,) functions as a parent or =
       localStorage.setItem('status', 'loggedIn') 
+      //We store a new string called username so that we can call out the specific username on other pages
+      localStorage.setItem('username', inputUsername)
       //Redirect to new page
       window.location = "menu.html";
         jQuery(window).load(function() {
@@ -71,12 +108,12 @@ submit.onclick = function(){
       // Return true to jump out of the function, since we now have all we need.
      return true;
     } 
-      //document.getElementById('loginResult').innerHTML = "Welcome " + localStorage.getItem('username') + "!";
        else {
       document.getElementById('loginResult').innerHTML = "Invalid login!";
      }
     
         
     
+}
 }
 
